73a111bcc13786a097dbac5fc34ff043877d0ace
Sebastian Giraldo <joabgiraldo@gmail.com>
2024-12-30 15:25:33 -0500

    feat: config next.config.ts cors

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async headers() {
+  async rewrites() {
     return [
-        {
-            source: process.env.NEXT_PUBLIC_API_URL || "",
-            headers: [
-                { key: "Access-Control-Allow-Credentials", value: "true" },
-                { key: "Access-Control-Allow-Origin", value: "*" },
-                { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-                { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-            ]
-        }
+      {
+        source: "/api/:path*",
+        destination: "https://api-task-manager-9xyj.onrender.com/api/:path*",
+      }
     ]
-}
+  }
 };
 
 export default nextConfig;
